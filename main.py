a2408c958f89cd0ed453f5c27eb544dae225fa23
JiankunW <jiankunwang62442@gmail.com>
2022-04-16 21:58:28 +0800

    add args

--- main.py
+++ main.py
@@ -7,14 +7,20 @@ from network import omicsGAN
 from preprocess import data_cleaning, optimize_data, significant_feats
 
 
-def main(total_update: int, dataset_name: str, m_select: int, mi_select) -> None:
+def main(
+    total_update: int,
+    dataset_name: str,
+    m_select: int,
+    mi_select: int,
+    test_threshold: float,
+    gpu_num: int,
+) -> None:
     mRNA_file = "data/" + dataset_name + "/mRNA.csv"
     miRNA_file = "data/" + dataset_name + "/miRNA.csv"
     adj_file = "data/" + dataset_name + "/adjacency.csv"
     label_file = "data/" + dataset_name + "/label.csv"
 
     torch.manual_seed(111)
-    gpu_num = 2
     device = torch.device(
         "cuda:" + str(gpu_num) if torch.cuda.is_available() else "cpu"
     )
@@ -23,20 +29,14 @@ def main(total_update: int, dataset_name: str, m_select: int, mi_select) -> None
     # INPUT
     data_original = data_cleaning(mRNA_file, miRNA_file, adj_file, label_file)
 
-    # Dataset: splitting dataset
-    # (To-DO)
-    # mRNA top3k.
-
-    # # of p-value <0.001
-
     # Optimized features
     num_feats = [m_select, mi_select]  # number of top features to select
     data = optimize_data(data_original, num_feats)
 
     # significant feature test (Table 4)
     print("Number of significant features...")
-    significant_feats(data["mRNA_data"], data["labels"], 0.05)
-    significant_feats(data["miRNA_data"], data["labels"], 0.05)
+    significant_feats(data["mRNA_data"], data["labels"], test_threshold)
+    significant_feats(data["miRNA_data"], data["labels"], test_threshold)
 
     # Evaluate single omic data
     auc_m = evaluate(data["mRNA_data"], data["labels"])
@@ -63,8 +63,8 @@ def main(total_update: int, dataset_name: str, m_select: int, mi_select) -> None
 
     # significant feature test (Table 4) after GANs
     print("Number of significant features...")
-    significant_feats(integrated_mRNA, data["labels"], 0.001)
-    significant_feats(integrated_miRNA, data["labels"], 0.001)
+    significant_feats(integrated_mRNA, data["labels"], test_threshold)
+    significant_feats(integrated_miRNA, data["labels"], test_threshold)
 
     # Save features
 
@@ -93,5 +93,16 @@ if __name__ == "__main__":
         help="# of selected features of mirna",
         default=256,
     )
+    parser.add_argument(
+        "-T",
+        "--test-threshold",
+        type=float,
+        help="p-value threshold for significant features",
+        default=0.001,
+    )
+    parser.add_argument(
+        "-g", "--gpu-num", type=int, help="gpu index", default=0,
+    )
     args = parser.parse_args()
+    print(args)
     main(**vars(args))
